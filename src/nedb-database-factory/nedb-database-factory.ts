61db0f4a8397ab07f09fc03da62d63c4b38aaccd
daria_smyr <dasha.smyr@gmail.com>
2023-03-22 15:57:42 +0300

    Small fix

--- src/nedb-database-factory/nedb-database-factory.ts
+++ src/nedb-database-factory/nedb-database-factory.ts
@@ -2,10 +2,6 @@ import Datastore from '@seald-io/nedb';
 
 export class NedbDatabaseFactory {
   private static datastores: Map<string, Datastore> = new Map();
-
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  private constructor() {}
-
   static create(datastoreFilePath: string): Datastore {
     if (this.datastores.has(datastoreFilePath)) {
       return this.datastores.get(datastoreFilePath) as Datastore;
