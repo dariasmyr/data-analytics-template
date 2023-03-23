e3e2e95cac48ffb9ce05efc8f820de947472ce5e
daria_smyr <dasha.smyr@gmail.com>
2023-03-23 15:57:03 +0300

    Fixed app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,8 @@ import { Logger } from './logger/logger';
 
 const logger = new Logger('App');
 
-function main(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  _arguments: string[],
-) {
+function main() {
   logger.debug('Starting the application');
 }
 
-main(process.argv);
+main();
